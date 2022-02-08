5520b312b141db5d6e4e85f054bce20109b10ee2
FelicianoSurya <suryamarcello@gmail.com>
2022-02-09 03:03:38 +0700

    weekly-meeting

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,4 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/weekly-meeting', [App\Http\Controllers\WeeklyMeeting::class, 'index'])->name('weeklymeeting');
